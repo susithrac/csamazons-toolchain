d4bf18f39e90f7d5b9092bd7bc165e471e16b4d4
Susithra Chandrabose <susithra_r@yahoo.com>
2021-06-17 10:27:44 +0530

    Calculation of CF

--- routes/users.js
+++ routes/users.js
@@ -25,23 +25,76 @@ async function getCloudant() {
         });
 
         router.post('/user', function (req, res, next) {
+            var copy = Object.assign({}, req.body);
+            //calculateCFData(copy);
             var document = req.body;
-            console.log(document);
-            try {
-                let data = db.insert(document);
-                res.send({ data: data });
-            } catch (err) {
-                console.log(err);
-                res.status(400).send('Could not insert the data');
-            }
+            var id = (document._id).split(":");
+            console.log(id);
+            var documentId = document._id;
+            var documentId = document.name;
+            delete document._id;
+            delete document.name;
+            var dataTobeAdded = document;
+            console.log("here" + id[1]);
+            db.find({ selector: { _id: "csamazon:" + id[1] } }, function (err, data) {
+                if (data && data.docs && data.docs.length > 0) {
+                    // req.body["_id"] = data.docs[0]["_id"];
+                    var newObject = {};
+                    newObject["_id"] = data.docs[0]["_id"];
+                    newObject["_rev"] = data.docs[0]["_rev"];
+                    let tempArray = data.docs[0][id[1]];
+
+                    tempArray.push(dataTobeAdded);
+                    console.log("New Temp Array");
+                    //console.log(tempArray);
+                    newObject[id[1]] = tempArray;
+                    console.log(newObject);
+                    if (data.docs[0]["_id"]) {
+                        db.insert(newObject, function (err, data1) {
+                            if (!err) {
+                                console.log('success', 'The record was updated successfully');
+                                res.status(200).send(data1);
+                            }
+                            else {
+                                console.log('failure', err);
+                                res.status(400).send("Unable to insert the data");
+                            }
+                        });
+                    } else {
+                        res.status(400).send("Unable to insert the data");
+                    }
+                } else {
+                    console.log("Unable to find the data1");
+                    console.log(copy);
+                    let data = db.insert(copy);
+                    if (data) {
+                        res.status(200).send({ data: data });
+                    } else {
+                        res.status(400).send("Error during saving the new data");
+                    }
+                }
+            });
         });
+        function calculateCFData(data) {
+            let housing = data["housing"];
+            housing["electricity"] *= 0.85;
+            housing["fuelOil"] *= 2.5;
+            //Remove natural gas
+            housing["lPG"] *= 2.983;
+            data["housing"]["CF"]=housing["electricity"]+housing["fuelOil"]+housing["lPG"]; 
+
+            let travel = data["travel"];
+            let food = data["food"];
+            let product = data["product"];
+            let services = data["services"];
+        }
 
         router.get('/:id', function (req, res, next) {
             console.log(req.params.id);
             db.find({ selector: { _id: "csamazon:" + req.params.id } }, function (err, data) {
                 if (!err) {
                     console.log(data);
-                    res.send(data);
+                    res.status(200).send(data);
                 } else {
                     res.status(400).send("Unable to find the data");
                 }
@@ -51,20 +104,24 @@ async function getCloudant() {
 
         router.put('/:id', function (req, res, next) {
             db.find({ selector: { _id: "csamazon:" + req.params.id } }, function (err, data) {
-                req.body["_id"] = data.docs[0]["_id"];
-                req.body["_rev"] = data.docs[0]["_rev"];
-                if (data.docs[0]["_id"]) {
-                    db.insert(req.body, function (err, data1) {
-                        if (!err) {
-                            console.log('success', 'The record was updated successfully');
-                            res.send(data1);
-                        }
-                        else {
-                            console.log('failure', err);
-                            res.status(400).send("Unable to insert the data");
-                        }
-                    });
-                }else{
+                if (data && data.docs && data.docs.length > 0) {
+                    req.body["_id"] = data.docs[0]["_id"];
+                    req.body["_rev"] = data.docs[0]["_rev"];
+                    if (data.docs[0]["_id"]) {
+                        db.insert(req.body, function (err, data1) {
+                            if (!err) {
+                                console.log('success', 'The record was updated successfully');
+                                res.status(200).send(data1);
+                            }
+                            else {
+                                console.log('failure', err);
+                                res.status(400).send("Unable to insert the data");
+                            }
+                        });
+                    } else {
+                        res.status(400).send("Unable to find the data");
+                    }
+                } else {
                     res.status(400).send("Unable to find the data");
                 }
             });
